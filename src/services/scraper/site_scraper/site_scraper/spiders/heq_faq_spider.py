a9ac6c4022945f8fcfc0e9399a203140e8044522
Nevil-ing <nevilpurpp12@gmail.com>
2025-06-02 23:14:13 +0300

    Refactor HefFaqSpider to remove unnecessary comments and improve code clarity

--- src/services/scraper/site_scraper/site_scraper/spiders/heq_faq_spider.py
+++ src/services/scraper/site_scraper/site_scraper/spiders/heq_faq_spider.py
@@ -7,8 +7,7 @@ class HefFaqSpider(scrapy.Spider):
 
     def __init__(self, site_url="https://www.hef.co.ke/faqs/", *args, **kwargs):
         super(HefFaqSpider, self).__init__(*args, **kwargs)
-        # For this specific spider, the site_url is usually fixed,
-        # but we can allow it to be overridden.
+        
         self.start_urls = [site_url]
         parsed_uri = urlparse(site_url)
         self.allowed_domains = [parsed_uri.netloc]
@@ -17,9 +16,7 @@ class HefFaqSpider(scrapy.Spider):
     def parse(self, response):
         self.logger.info(f"Scraping {response.url}")
 
-        # 1. Page Title (from <title> tag - Scrapy does this by default, but good to have)
-        # The actual title might be in an <h1> or a specific Elementor heading widget
-        # For <title> tag:
+       
         html_page_title = response.css('title::text').get()
         
         # Main heading of the FAQ section
@@ -36,20 +33,14 @@ class HefFaqSpider(scrapy.Spider):
                     'text': text.strip(),
                     'href': response.urljoin(href.strip())
                 })
-
-        # 3. FAQ Items (Questions and Answers)
-        # This site has two sets of toggles with slightly different widget IDs but similar structure
         faq_items = []
-        # Targeting both toggle widgets (elementor-element-16e6739 and elementor-element-e387d3f)
+       
         for toggle_widget in response.css('div.elementor-widget-toggle'):
             for item in toggle_widget.css('div.elementor-toggle-item'):
                 question = item.css('div.elementor-tab-title a.elementor-toggle-title::text').get()
                 if not question: # Fallback if <a> tag is not directly there or text is outside
                     question = "".join(item.css('div.elementor-tab-title ::text').getall()).strip()
 
-
-                # The answer content can have <p> tags or just be text.
-                # We select all text nodes within the content div and join them.
                 answer_parts = item.css('div.elementor-tab-content ::text').getall()
                 answer = ' '.join(part.strip() for part in answer_parts if part.strip())
 
